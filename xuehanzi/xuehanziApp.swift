7725f0dae77501052de3c699ef9d614f5eeb1b11
Evangelos Meklis <vmeklis@hotmail.com>
2026-02-01 20:30:59 +0200

    Initial Commit

---
+++ xuehanzi/xuehanziApp.swift
@@ -0,0 +1,17 @@
+//
+//  xuehanziApp.swift
+//  xuehanzi
+//
+//  Created by Evangelos Meklis on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct xuehanziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xuehanziTests/xuehanziTests.swift
@@ -0,0 +1,17 @@
+//
+//  xuehanziTests.swift
+//  xuehanziTests
+//
+//  Created by Evangelos Meklis on 1/2/26.
+//
+
+import Testing
+@testable import xuehanzi
+
+struct xuehanziTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
